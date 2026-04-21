6fbddf77fb0a66ea2f8f4c064fda24207b01cb6a
kishan mandal <mandalk619@gmail.com>
2026-04-21 20:54:59 +0530

    Fixed proxy issue for Render (419 error resolved)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,5 +16,8 @@ class AppServiceProvider extends ServiceProvider
         /** @var Router $router */
         $router = $this->app['router'];
         $router->aliasMiddleware('admin.auth', \App\Http\Middleware\AdminAuth::class);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
